3390758b08c92e6bc136eb596011848afe75700e
manpreet <pb26manpreet@gmail.com>
2025-11-11 22:08:01 +0530

    hello

---
+++ dictonary.py
@@ -0,0 +1,48 @@
+# info = {
+#     "key" : "value",
+#     "name" : "apnacollege",
+#     "learning" : "coding"
+# }
+# print(info)
+
+
+
+
+# info = {
+#     "name" : "apnacollege",
+#     "subject" : ["python","c","java"],
+#     "topics" : ["dict","set"],
+#     "age" : 35,
+#     "is_adult" : True
+#     "marks" : 94.4 #12.99:94.4
+#     print(info)
+#     print(type(info))
+# }
+# print(info)
+
+
+
+
+
+# nfo = {
+#     "name" : "apnacollege",
+#     "subject" : ["python","c","java"],
+#     "topics" : ["dict","set"],
+#     "age" : 35,
+#     "is_adult" : True
+#     12.99 : 94.4
+    
+    
+# }
+# print(info["name"])
+# print(info["topics"])
+# print(nfo["subject"])
+# print(info["age"])
+
+
+
+
+# null_dict = {}
+# null_dict["name"] = "apnacollege"
+# print(null_dict)
+
