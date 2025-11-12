f89ef14a983c75b90dcd3cca29d949371f06e65b
manpreet <pb26manpreet@gmail.com>
2025-11-12 16:44:10 +0530

    set_method

---
+++ set_methods.py
@@ -0,0 +1,22 @@
+# # add method
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(3)
+# collection.add(4)
+
+# print(collection)
+
+# # remove method
+
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(3)
+# collection.add(4)
+
+# collection.remove(2)
+# print(collection)
+
