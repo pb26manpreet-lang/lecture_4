43b3863f1715b3e4df16850191ca7f6e0859f8c1
manpreet <pb26manpreet@gmail.com>
2025-11-13 17:07:36 +0530

    set_method_too

---
+++ set_methods_to.py
@@ -0,0 +1,16 @@
+# union method
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+
+print(set1.union(set2))
+
+
+#  intersetion method
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+
+print(set1.intersection(set2))
+print(set1)
+print(set2)
