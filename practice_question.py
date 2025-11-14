402650efd4fbb445dc568164e3a53f907a103796
manpreet <pb26manpreet@gmail.com>
2025-11-14 13:58:37 +0530

    questions

---
+++ practice_question.py
@@ -0,0 +1,18 @@
+# # question_number first
+
+# dictionary = {
+#     "cat" = "a small animal",
+#     "table" = ["a piece of furniture","list of facts & figures"]
+# }
+
+# print(dictionary)
+
+
+# # question_number second
+
+# subjects = {
+#     "python","java","c++","python","javascript","java",
+#     "python","java","c++","c"
+# }
+
+# print(len(subjects))
