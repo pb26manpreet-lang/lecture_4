623d44447013c1fc875cbd0880171c813d03d9e2
manpreet <pb26manpreet@gmail.com>
2025-11-18 22:09:10 +0530

    lets_pra_question.py

---
+++ let_practice_question.py
@@ -0,0 +1,27 @@
+# # first question solve
+
+# marks = {}
+# x = int(input("enter phy :"))
+# marks.update({"phy" : x })
+
+# x = int(input("enter math :"))
+# marks.update({"math" : x })
+
+# x = int(input("enter chem:"))
+# marks.update({"chem" : x })
+
+# print(marks)
+
+
+# # second question solve
+
+# values = {9,9.25,8,8.03}
+# print(marks)
+
+# # second question solve
+
+# values = {
+#     ("float",9.0)
+#     ("int",9)    
+# }
+# print(values)
